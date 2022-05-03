76b95c69c565fe507d459888eb31dbaa60741803
javadevAK <java.devlearn@gmail.com>
2022-05-03 08:00:22 +0300

    update server socket

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,41 +12,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         //System.out.println(engine.search("бизнес"));
-
-
         int port = 8989;
 
-        ServerSocket serverSocket = new ServerSocket(port);
-        //try (ServerSocket serverSocket = new ServerSocket(port);
-        //ждём подключения, когда происходит соединение
-        try (Socket clientSocket = serverSocket.accept();
-             //    Socket clientSocket = serverSocket.accept();
-             // отправлять
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             // получать сообщения
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-
-            System.out.printf("New connection accepted. Port %d%n", clientSocket.getPort());
-
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            //ждём подключения, когда происходит соединение
             while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     // отправлять
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     // получать сообщения
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-                // ждем клиента
-                final String wordSearch = in.readLine();
-                //String wordSearch = "бизнес";
-                // отвечаем клиенту
-                List<PageEntry> list = engine.search(wordSearch);
-                serverSocket.close();
-                GsonBuilder builder = new GsonBuilder();
-                Gson gson = builder.create();
-                Type listType = new TypeToken<List<PageEntry>>() {
-                }.getType();
+                    System.out.printf("New connection accepted. Port %d%n", clientSocket.getPort());
+                    // ждем клиента
+                    final String wordSearch = in.readLine();
+                    //String wordSearch = "бизнес";
+                    // отвечаем клиенту
+                    List<PageEntry> list = engine.search(wordSearch);
+                    GsonBuilder builder = new GsonBuilder();
+                    Gson gson = builder.create();
+                    Type listType = new TypeToken<List<PageEntry>>() {
+                    }.getType();
 
-                out.println(gson.toJson(list, listType));
+                    out.println(gson.toJson(list, listType));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
